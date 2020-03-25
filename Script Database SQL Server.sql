14edea63d71141e4238b99e46df0fd8608298543
luispedrotrindade <lp_tp@hotmail.com>
2020-03-25 13:03:07 -0300

    #

--- Script Database SQL Server.sql
+++ Script Database SQL Server.sql
@@ -1,7 +1,7 @@
 create database teste_webmotors
+go
+use teste_webmotors
 go 
-
-use teste_webmotors;
 
 create table tb_AnuncioWebmotors(
 ID INT identity not null,
@@ -13,3 +13,88 @@ quilometragem INT not null,
 observacao text not null 
 )
 go
+
+
+create procedure CreateAnuncio
+(
+@marca			varchar(45),
+@modelo			varchar(45),
+@versao			varchar(45),
+@ano			int,
+@quilometragem	int,
+@observacao		text
+)
+as
+Begin 
+Insert into tb_AnuncioWebmotors (
+				 marca			
+				,modelo			
+				,versao			
+				,ano			
+				,quilometragem	
+				,observacao		
+			)
+			values(
+				 @marca			
+				,@modelo			
+				,@versao			
+				,@ano			
+				,@quilometragem	
+				,@observacao		
+			)
+end
+
+go 
+
+create procedure ListAll
+as
+Begin
+select * from tb_AnuncioWebmotors
+end
+
+go
+
+create procedure UpdateAnuncio
+(
+@id				int,
+@marca			varchar(45),
+@modelo			varchar(45),
+@versao			varchar(45),
+@ano			int,
+@quilometragem	int,
+@observacao		text
+)
+as
+Begin 
+update  tb_AnuncioWebmotors set 
+						 marca			= @marca			
+						,modelo			= @modelo			
+						,versao			= @versao			
+						,ano			= @ano				
+						,quilometragem	= @quilometragem	
+						,observacao		= @observacao		
+
+		where ID = @id
+end
+
+go
+
+create procedure DeleteAnuncio
+(
+@id int
+)
+as
+Begin 
+delete from  tb_AnuncioWebmotors where id = @id
+end
+
+go
+
+create procedure GetById
+(
+@id int
+)
+as
+Begin 
+select * from  tb_AnuncioWebmotors  where ID = @id
+end
